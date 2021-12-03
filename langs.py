0db43d63293c7ebcc1e62c55d914f72492ec7c23
xshadowsexy <82493057+Sweety187@users.noreply.github.com>
2021-12-03 21:08:43 +0700

    Update langs.py

--- langs.py
+++ langs.py
@@ -38,19 +38,19 @@ languages = {
 		"restarting": "{} Перезапуск...",
 		"elem_setto": "Элемент с индексом {} установлен на значение `{}`.",
 		"elem_deleted": "Элемент с индексом {} удален.",
-		"status_pattern": """Серверов: {}
-Пользователей: {}
-Вызовов сканера: {}
-Удалено сообщений: {}
-Клиент: {}
-ID: {}
-Оперативная память: {}%
-Процессор: {}%
-Аптайм: {}
-Задержка вебсокета: {} мс.
-Python: {}
-discord.py: {}
-Wolverine: {}""",
+		"status_pattern": """**Серверов**: {}
+**Пользователей**: {}
+**Вызовов сканера**: {}
+**Удалено сообщений**: {}
+**Клиент**: {}
+**ID**: {}
+**Оперативная память**: {}%
+**Процессор**: {}%
+**Аптайм**: {}
+**Задержка вебсокета**: {} мс.
+**Python**: {}
+**discord.py**: {}
+**Wolverine**: {}""",
 		"bot_status": ":satellite: Состояние бота",
 		"info": "Информация",
 		"about": "{} Данный бот предназначен для защиты вашего сервера от скама с «Бесплатным Nitro на 3 месяца от Steam» и людьми якобы раздающими свой инвентарь CS:GO. Если вы увидите подобные сообщения, не ведитесь на них!\n\n{} Что-бы ваш аккаунт не взломали, не используйте BetterDiscord и не загружайте подозрительное ПО. Если же вас уже взломали, рекомендуем вам выполнить следующие действия:\nㆍУдалите BetterDiscord с вашего устройства;\nㆍПереустановите клиент Discord;\nㆍПоменяйте пароли всех ваших аккаунтов;\nㆍУстановите надежный антивирус и выполните полную проверку устройства.\n\nБерегите себя!\n\n**Версия ядра**: [Wolverine {}](https://scamprotect.ml/wolverine)\n**Разработчик**: [xshadowsexy#0141](https://discord.com/users/811976103673593856)\n**Вебсайт**: https://scamprotect.ml/\n**Наш сервер**: https://discord.gg/GpedR6jeZR\n**Пожертвовать**: https://qiwi.com/n/XF765",
@@ -123,19 +123,19 @@ Wolverine: {}""",
 		"restarting": "{} Restarting...",
 		"elem_setto": "Element with index {} has been set to `{}`.",
 		"elem_deleted": "Element with index {} has been deleted.",
-		"status_pattern": """Servers: {}
-Users: {}
-Scanner calls: {}
-Messages deleted: {}
-Client: {}
-ID: {}
-RAM load: {}%
-CPU load: {}%
-Uptime: {}
-Websocket ping: {} ms.
-Python: {}
-discord.py: {}
-Wolverine: {}""",
+		"status_pattern": """**Servers**: {}
+**Users**: {}
+**Scanner calls**: {}
+**Messages deleted**: {}
+**Client**: {}
+**ID**: {}
+**RAM load**: {}%
+**CPU load**: {}%
+**Uptime**: {}
+**Websocket ping**: {} ms.
+**Python**: {}
+**discord.py**: {}
+**Wolverine**: {}""",
 		"bot_status": ":satellite: Bot status",
 		"info": "Information",
 		"about": "{} This bot is made to protect your server from «Free Nitro for 3 months from Steam» and people allegedly «distributing their CS:GO inventory» scams. If you see similar messages, don't get fooled by them!\n\n{} To prevent your account from being hacked, do not use BetterDiscord and do not download suspicious software. If you have already been hacked, we recommend you follow these steps:\nㆍRemove BetterDiscord from your device;\nㆍReinstall the Discord client;\nㆍChange the passwords of all your accounts;\nㆍInstall a reliable antivirus and perform a full device scan.\n\n Take care of yourself!\n\n**Core version**: [Wolverine {}](https://scamprotect.ml/wolverine )\n**Developer**: [xshadowsexy#0141](https://discord.com/users/811976103673593856 )\n**Source code**: [Outdated public version](https://github.com/ezz-dev/scamprotect )\n**Website**: https://scamprotect.ml/\n**Our server**: https://discord.gg/GpedR6jeZR\n**Donate**: https://qiwi.com/n/XF765",
